371569d3ad875fda6137d3642819c055d00e17d7
b4bulle <37732841+b4bulle@users.noreply.github.com>
2024-05-27 23:51:39 +0200

    open function to everyone

--- Babulle.Bullebot.TwitchFunctions/TwitchEvent.cs
+++ Babulle.Bullebot.TwitchFunctions/TwitchEvent.cs
@@ -12,7 +12,7 @@ public class TwitchEvent(ILoggerFactory loggerFactory, IMediator mediator)
     private readonly ILogger _logger = loggerFactory.CreateLogger<TwitchEvent>();
 
     [Function("TwitchEvent")]
-    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequestData req,
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req,
         FunctionContext executionContext)
     {
         _logger.LogInformation("C# HTTP trigger function processed a request.");
